c56318d1d197e29450f65e372bca8daa3c2bf929
Jolken <jolken122@gmail.com>
2018-06-19 00:29:40 +0300

    moved to firefox

--- content.js
+++ content.js
@@ -1,3 +1,24 @@
-window.onhashchange = function() {
-     alert(1);
+browser.contextMenus.create({
+    id: "eat-page",
+    title: "Съесть эту страницу"
+});
+
+function messageTab(tabs) {
+    browser.tabs.sendMessage(tabs[0].id, {
+        replacement: "Message from the extension!"
+    });
 }
+
+browser.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId == "eat-page") {
+        browser.tabs.executeScript({
+            file: "page-eater.js"
+        });
+
+        var querying = browser.tabs.query({
+            active: true,
+            currentWindow: true
+        });
+        querying.then(messageTab);
+    }
+});
