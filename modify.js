394de05f8ada818ec71b8ec0227b23319b15df1f
Jolken <jolken122@gmail.com>
2018-06-21 00:55:59 +0300

    undefined

--- modify.js
+++ modify.js
@@ -4,19 +4,31 @@ function identifyRequest(request, sender, sendResponse){
         modify();
     }
     else if(request.action == 'call') {
-        call();
+        call(5);
     }
 }
+
 function modify () {
     var chats = document.querySelectorAll('.containerDefault-1ZnADq');
     chats.forEach((chat) => {
         if (!chat.childNodes[0].childNodes[0].childNodes[0].innerHTML.includes('<input type="checkbox">') & chat.childNodes[0].childNodes[0].childNodes[0].innerHTML.includes('Voice')){
             chat.childNodes[0].childNodes[0].childNodes[0].innerHTML += '<input type="checkbox">';
-            checkboxes.push(chat.childNodes[0].childNodes[0].childNodes[0]);
+            checkboxes.push(chat.childNodes[0].childNodes[0].childNodes[0].childNodes[1]);
+            checkboxes.filter(checkbox => checkbox);
         }
     });
     console.log(checkboxes);
 }
+
+function call(times) {
+    checkboxes.filter(checkbox => checkbox.checked);
+    for (let i = 1; i >= times; i++) {
+        checkboxes.forEach((checkbox) => {
+            checkbox.click();
+        });
+    }
+}
+
 browser.runtime.onMessage.addListener(identifyRequest)
 
 
