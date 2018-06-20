6232cb00909a11b0bce422a336ebcf73ba800c62
Jolken <jolken122@gmail.com>
2018-06-20 22:57:09 +0300

    sendmessage api

--- content.js
+++ content.js
@@ -1,5 +1,17 @@
+var action = 'modify';
+function request(tabs) {
+    browser.tabs.sendMessage(tabs[0].id, {
+        'action': action
+    });
+}
+
 browser.browserAction.onClicked.addListener((tab) => {
     browser.tabs.executeScript({
         file: './modify.js'
     });
+    var querying = browser.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+    querying.then(request);
 });
--- modify.js
+++ modify.js
@@ -1,6 +1,23 @@
-var chats = document.querySelectorAll('.containerDefault-1ZnADq');
-chats.forEach((chat) => {
-    chat.childNodes[0].childNodes[0].childNodes[1].innerHTML += '<input type="checkbox">';
-});
+var checkboxes = [];
+function identifyRequest(request, sender, sendResponse){
+    if (request.action == 'modify') {
+        modify();
+    }
+    else if(request.action == 'call') {
+        call();
+    }
+}
+function modify () {
+    var chats = document.querySelectorAll('.containerDefault-1ZnADq');
+    chats.forEach((chat) => {
+        if (!chat.childNodes[0].childNodes[0].childNodes[0].innerHTML.includes('<input type="checkbox">') & chat.childNodes[0].childNodes[0].childNodes[0].innerHTML.includes('Voice')){
+            chat.childNodes[0].childNodes[0].childNodes[0].innerHTML += '<input type="checkbox">';
+            checkboxes.push(chat.childNodes[0].childNodes[0].childNodes[0]);
+        }
+    });
+    console.log(checkboxes);
+}
+browser.runtime.onMessage.addListener(identifyRequest)
+
 
 
